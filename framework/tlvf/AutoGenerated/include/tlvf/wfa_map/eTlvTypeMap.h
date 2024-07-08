1b13563a248cc8ffb10a5a8ee0dd06b49b51bb2b
HENRIQUES Nicolas <prplmesh.opensources@sagemcom.com>
2024-07-08 15:45:40 +0200

    tlvf: tid to link mapping policy refacto
    Signed-off-by: HENRIQUES Nicolas <prplmesh.opensources@sagemcom.com>

--- framework/tlvf/AutoGenerated/include/tlvf/wfa_map/eTlvTypeMap.h
+++ framework/tlvf/AutoGenerated/include/tlvf/wfa_map/eTlvTypeMap.h
@@ -121,9 +121,9 @@ enum class eTlvTypeMap : uint8_t {
     TLV_MLD_STRUCTURE = 0xe3,
     TLV_AFFILIATED_STA_METRICS = 0xe4,
     TLV_AFFILIATED_AP_METRICS = 0xe5,
+    TLV_TID_TO_LINK_MAPPING_POLICY = 0xe6,
     TLV_EHT_OPERATIONS = 0xe7,
     TLV_AP_WIFI_6_CAPABILITIES = 0xaa,
-    TLV_TID_TO_LINK_MAPPING_POLICY = 0xe6,
 };
 // Enum AutoPrint generated code snippet begining- DON'T EDIT!
 // clang-format off
@@ -229,9 +229,9 @@ static const char *eTlvTypeMap_str(eTlvTypeMap enum_value) {
     case eTlvTypeMap::TLV_MLD_STRUCTURE:                            return "eTlvTypeMap::TLV_MLD_STRUCTURE";
     case eTlvTypeMap::TLV_AFFILIATED_STA_METRICS:                   return "eTlvTypeMap::TLV_AFFILIATED_STA_METRICS";
     case eTlvTypeMap::TLV_AFFILIATED_AP_METRICS:                    return "eTlvTypeMap::TLV_AFFILIATED_AP_METRICS";
+    case eTlvTypeMap::TLV_TID_TO_LINK_MAPPING_POLICY:               return "eTlvTypeMap::TLV_TID_TO_LINK_MAPPING_POLICY";
     case eTlvTypeMap::TLV_EHT_OPERATIONS:                           return "eTlvTypeMap::TLV_EHT_OPERATIONS";
     case eTlvTypeMap::TLV_AP_WIFI_6_CAPABILITIES:                   return "eTlvTypeMap::TLV_AP_WIFI_6_CAPABILITIES";
-    case eTlvTypeMap::TLV_TID_TO_LINK_MAPPING_POLICY:               return "eTlvTypeMap::TLV_TID_TO_LINK_MAPPING_POLICY";
     }
     static std::string out_str = std::to_string(int(enum_value));
     return out_str.c_str();
@@ -344,9 +344,9 @@ public:
         case 0xe3:
         case 0xe4:
         case 0xe5:
+        case 0xe6:
         case 0xe7:
         case 0xaa:
-        case 0xe6:
                 ret = true;
                 break;
             default:

5c57231b6673a138b4d6251446b3615240acb74c
Pallapu Karthik <pkarthik@maxlinear.com>
2024-07-10 14:26:10 +0800

    tlvf: add autogenerated files for tlvAffiliatedApMetrics
    Add the autogenerated code cpp and h files for the tlvAffiliatedApMetrics
    which contains the required attributes and functions.
    
    Signed-off-by: Pallapu Karthik <pkarthik@maxlinear.com>

--- framework/tlvf/AutoGenerated/include/tlvf/wfa_map/eTlvTypeMap.h
+++ framework/tlvf/AutoGenerated/include/tlvf/wfa_map/eTlvTypeMap.h
@@ -120,6 +120,7 @@ enum class eTlvTypeMap : uint8_t {
     TLV_MLD_STRUCTURE = 0xe3,
     TLV_EHT_OPERATIONS = 0xe7,
     TLV_AP_WIFI_6_CAPABILITIES = 0xaa,
+    TLV_AFFILIATED_AP_METRICS = 0xe5,
 };
 // Enum AutoPrint generated code snippet begining- DON'T EDIT!
 // clang-format off
@@ -224,6 +225,7 @@ static const char *eTlvTypeMap_str(eTlvTypeMap enum_value) {
     case eTlvTypeMap::TLV_MLD_STRUCTURE:                            return "eTlvTypeMap::TLV_MLD_STRUCTURE";
     case eTlvTypeMap::TLV_EHT_OPERATIONS:                           return "eTlvTypeMap::TLV_EHT_OPERATIONS";
     case eTlvTypeMap::TLV_AP_WIFI_6_CAPABILITIES:                   return "eTlvTypeMap::TLV_AP_WIFI_6_CAPABILITIES";
+    case eTlvTypeMap::TLV_AFFILIATED_AP_METRICS:                    return "eTlvTypeMap::TLV_AFFILIATED_AP_METRICS";
     }
     static std::string out_str = std::to_string(int(enum_value));
     return out_str.c_str();
@@ -335,6 +337,7 @@ public:
         case 0xe3:
         case 0xe7:
         case 0xaa:
+        case 0xe5:
                 ret = true;
                 break;
             default:

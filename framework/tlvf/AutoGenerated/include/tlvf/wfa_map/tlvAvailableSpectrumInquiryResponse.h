AnoojBiju/Openwrt-Prplmesh

tlvf: add autogenerated files for tlvAvailableSpectrumInquiryResponse
Add the autogenerated code cpp and h files for the tlvAvailableSpectrumInquiryResponse
which contains the required attributes and functions.

Signed-off-by: Pallapu Karthik <pkarthik@maxlinear.com>

framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvAvailableSpectrumInquiryResponse.h
@@ -1,0 +1,57 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_WFA_MAP_TLVAVAILABLESPECTRUMINQUIRYRESPONSE_H_
+#define _TLVF_WFA_MAP_TLVAVAILABLESPECTRUMINQUIRYRESPONSE_H_
+
+#include <cstddef>
+#include <stdint.h>
+#include <tlvf/swap.h>
+#include <string.h>
+#include <memory>
+#include <tlvf/BaseClass.h>
+#include <tlvf/ClassList.h>
+#include "tlvf/wfa_map/eTlvTypeMap.h"
+#include <tuple>
+
+namespace wfa_map {
+
+
+class tlvAvailableSpectrumInquiryResponse : public BaseClass
+{
+    public:
+        tlvAvailableSpectrumInquiryResponse(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit tlvAvailableSpectrumInquiryResponse(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~tlvAvailableSpectrumInquiryResponse();
+
+        const eTlvTypeMap& type();
+        const uint16_t& length();
+        size_t avai_spec_inquiry_response_obj_length() { return m_avai_spec_inquiry_response_obj_idx__ * sizeof(uint8_t); }
+        uint8_t* avai_spec_inquiry_response_obj(size_t idx = 0);
+        bool set_avai_spec_inquiry_response_obj(const void* buffer, size_t size);
+        bool alloc_avai_spec_inquiry_response_obj(size_t count = 1);
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eTlvTypeMap* m_type = nullptr;
+        uint16_t* m_length = nullptr;
+        uint8_t* m_avai_spec_inquiry_response_obj = nullptr;
+        size_t m_avai_spec_inquiry_response_obj_idx__ = 0;
+        int m_lock_order_counter__ = 0;
+};
+
+}; // close namespace: wfa_map
+
+#endif //_TLVF/WFA_MAP_TLVAVAILABLESPECTRUMINQUIRYRESPONSE_H_

framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvAvailableSpectrumInquiryResponse.cpp
@@ -1,0 +1,164 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <tlvf/wfa_map/tlvAvailableSpectrumInquiryResponse.h>
+#include <tlvf/tlvflogging.h>
+
+using namespace wfa_map;
+
+tlvAvailableSpectrumInquiryResponse::tlvAvailableSpectrumInquiryResponse(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+tlvAvailableSpectrumInquiryResponse::tlvAvailableSpectrumInquiryResponse(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+tlvAvailableSpectrumInquiryResponse::~tlvAvailableSpectrumInquiryResponse() {
+}
+const eTlvTypeMap& tlvAvailableSpectrumInquiryResponse::type() {
+    return (const eTlvTypeMap&)(*m_type);
+}
+
+const uint16_t& tlvAvailableSpectrumInquiryResponse::length() {
+    return (const uint16_t&)(*m_length);
+}
+
+uint8_t* tlvAvailableSpectrumInquiryResponse::avai_spec_inquiry_response_obj(size_t idx) {
+    if ( (m_avai_spec_inquiry_response_obj_idx__ == 0) || (m_avai_spec_inquiry_response_obj_idx__ <= idx) ) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+        return nullptr;
+    }
+    return &(m_avai_spec_inquiry_response_obj[idx]);
+}
+
+bool tlvAvailableSpectrumInquiryResponse::set_avai_spec_inquiry_response_obj(const void* buffer, size_t size) {
+    if (buffer == nullptr) {
+        TLVF_LOG(WARNING) << "set_avai_spec_inquiry_response_obj received a null pointer.";
+        return false;
+    }
+    if (m_avai_spec_inquiry_response_obj_idx__ != 0) {
+        TLVF_LOG(ERROR) << "set_avai_spec_inquiry_response_obj was already allocated!";
+        return false;
+    }
+    if (!alloc_avai_spec_inquiry_response_obj(size)) { return false; }
+    std::copy_n(reinterpret_cast<const uint8_t *>(buffer), size, m_avai_spec_inquiry_response_obj);
+    return true;
+}
+bool tlvAvailableSpectrumInquiryResponse::alloc_avai_spec_inquiry_response_obj(size_t count) {
+    if (m_lock_order_counter__ > 0) {;
+        TLVF_LOG(ERROR) << "Out of order allocation for variable length list avai_spec_inquiry_response_obj, abort!";
+        return false;
+    }
+    size_t len = sizeof(uint8_t) * count;
+    if(getBuffRemainingBytes() < len )  {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer - can't allocate";
+        return false;
+    }
+    m_lock_order_counter__ = 0;
+    uint8_t *src = (uint8_t *)&m_avai_spec_inquiry_response_obj[m_avai_spec_inquiry_response_obj_idx__];
+    uint8_t *dst = src + len;
+    if (!m_parse__) {
+        size_t move_length = getBuffRemainingBytes(src) - len;
+        std::copy_n(src, move_length, dst);
+    }
+    m_avai_spec_inquiry_response_obj_idx__ += count;
+    if (!buffPtrIncrementSafe(len)) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+        return false;
+    }
+    if(m_length){ (*m_length) += len; }
+    return true;
+}
+
+void tlvAvailableSpectrumInquiryResponse::class_swap()
+{
+    tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
+}
+
+bool tlvAvailableSpectrumInquiryResponse::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+        *m_length -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t tlvAvailableSpectrumInquiryResponse::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(eTlvTypeMap); // type
+    class_size += sizeof(uint16_t); // length
+    return class_size;
+}
+
+bool tlvAvailableSpectrumInquiryResponse::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_type = reinterpret_cast<eTlvTypeMap*>(m_buff_ptr__);
+    if (!m_parse__) *m_type = eTlvTypeMap::TLV_AVAILABLE_SPECTRUM_INQUIRY_RESPONSE;
+    if (!buffPtrIncrementSafe(sizeof(eTlvTypeMap))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(eTlvTypeMap) << ") Failed!";
+        return false;
+    }
+    m_length = reinterpret_cast<uint16_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_length = 0;
+    if (!buffPtrIncrementSafe(sizeof(uint16_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint16_t) << ") Failed!";
+        return false;
+    }
+    m_avai_spec_inquiry_response_obj = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (m_length && m_parse__) {
+        auto swap_len = *m_length;
+        tlvf_swap((sizeof(swap_len) * 8), reinterpret_cast<uint8_t*>(&swap_len));
+        size_t len = swap_len;
+        len -= (m_buff_ptr__ - sizeof(*m_type) - sizeof(*m_length) - m_buff__);
+        m_avai_spec_inquiry_response_obj_idx__ = len/sizeof(uint8_t);
+        if (!buffPtrIncrementSafe(len)) {
+            LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+            return false;
+        }
+    }
+    if (m_parse__) { class_swap(); }
+    if (m_parse__) {
+        if (*m_type != eTlvTypeMap::TLV_AVAILABLE_SPECTRUM_INQUIRY_RESPONSE) {
+            TLVF_LOG(ERROR) << "TLV type mismatch. Expected value: " << int(eTlvTypeMap::TLV_AVAILABLE_SPECTRUM_INQUIRY_RESPONSE) << ", received value: " << int(*m_type);
+            return false;
+        }
+    }
+    return true;
+}
+
+

mujtahid-akon/myPrplMesh

framework/tlvf/AutoGenerated/include/tlvf/wfa_map/tlvAvailableSpectrumInquiryResponse.h
@@ -1,0 +1,57 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#ifndef _TLVF_WFA_MAP_TLVAVAILABLESPECTRUMINQUIRYRESPONSE_H_
+#define _TLVF_WFA_MAP_TLVAVAILABLESPECTRUMINQUIRYRESPONSE_H_
+
+#include <cstddef>
+#include <stdint.h>
+#include <tlvf/swap.h>
+#include <string.h>
+#include <memory>
+#include <tlvf/BaseClass.h>
+#include <tlvf/ClassList.h>
+#include "tlvf/wfa_map/eTlvTypeMap.h"
+#include <tuple>
+
+namespace wfa_map {
+
+
+class tlvAvailableSpectrumInquiryResponse : public BaseClass
+{
+    public:
+        tlvAvailableSpectrumInquiryResponse(uint8_t* buff, size_t buff_len, bool parse = false);
+        explicit tlvAvailableSpectrumInquiryResponse(std::shared_ptr<BaseClass> base, bool parse = false);
+        ~tlvAvailableSpectrumInquiryResponse();
+
+        const eTlvTypeMap& type();
+        const uint16_t& length();
+        size_t available_spectrum_inquiry_response_obj_length() { return m_available_spectrum_inquiry_response_obj_idx__ * sizeof(uint8_t); }
+        uint8_t* available_spectrum_inquiry_response_obj(size_t idx = 0);
+        bool set_available_spectrum_inquiry_response_obj(const void* buffer, size_t size);
+        bool alloc_available_spectrum_inquiry_response_obj(size_t count = 1);
+        void class_swap() override;
+        bool finalize() override;
+        static size_t get_initial_size();
+
+    private:
+        bool init();
+        eTlvTypeMap* m_type = nullptr;
+        uint16_t* m_length = nullptr;
+        uint8_t* m_available_spectrum_inquiry_response_obj = nullptr;
+        size_t m_available_spectrum_inquiry_response_obj_idx__ = 0;
+        int m_lock_order_counter__ = 0;
+};
+
+}; // close namespace: wfa_map
+
+#endif //_TLVF/WFA_MAP_TLVAVAILABLESPECTRUMINQUIRYRESPONSE_H_

framework/tlvf/AutoGenerated/src/tlvf/wfa_map/tlvAvailableSpectrumInquiryResponse.cpp
@@ -1,0 +1,164 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * SPDX-FileCopyrightText: 2016-2020 the prplMesh contributors (see AUTHORS.md)
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <tlvf/wfa_map/tlvAvailableSpectrumInquiryResponse.h>
+#include <tlvf/tlvflogging.h>
+
+using namespace wfa_map;
+
+tlvAvailableSpectrumInquiryResponse::tlvAvailableSpectrumInquiryResponse(uint8_t* buff, size_t buff_len, bool parse) :
+    BaseClass(buff, buff_len, parse) {
+    m_init_succeeded = init();
+}
+tlvAvailableSpectrumInquiryResponse::tlvAvailableSpectrumInquiryResponse(std::shared_ptr<BaseClass> base, bool parse) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse){
+    m_init_succeeded = init();
+}
+tlvAvailableSpectrumInquiryResponse::~tlvAvailableSpectrumInquiryResponse() {
+}
+const eTlvTypeMap& tlvAvailableSpectrumInquiryResponse::type() {
+    return (const eTlvTypeMap&)(*m_type);
+}
+
+const uint16_t& tlvAvailableSpectrumInquiryResponse::length() {
+    return (const uint16_t&)(*m_length);
+}
+
+uint8_t* tlvAvailableSpectrumInquiryResponse::available_spectrum_inquiry_response_obj(size_t idx) {
+    if ( (m_available_spectrum_inquiry_response_obj_idx__ == 0) || (m_available_spectrum_inquiry_response_obj_idx__ <= idx) ) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+        return nullptr;
+    }
+    return &(m_available_spectrum_inquiry_response_obj[idx]);
+}
+
+bool tlvAvailableSpectrumInquiryResponse::set_available_spectrum_inquiry_response_obj(const void* buffer, size_t size) {
+    if (buffer == nullptr) {
+        TLVF_LOG(WARNING) << "set_available_spectrum_inquiry_response_obj received a null pointer.";
+        return false;
+    }
+    if (m_available_spectrum_inquiry_response_obj_idx__ != 0) {
+        TLVF_LOG(ERROR) << "set_available_spectrum_inquiry_response_obj was already allocated!";
+        return false;
+    }
+    if (!alloc_available_spectrum_inquiry_response_obj(size)) { return false; }
+    std::copy_n(reinterpret_cast<const uint8_t *>(buffer), size, m_available_spectrum_inquiry_response_obj);
+    return true;
+}
+bool tlvAvailableSpectrumInquiryResponse::alloc_available_spectrum_inquiry_response_obj(size_t count) {
+    if (m_lock_order_counter__ > 0) {;
+        TLVF_LOG(ERROR) << "Out of order allocation for variable length list available_spectrum_inquiry_response_obj, abort!";
+        return false;
+    }
+    size_t len = sizeof(uint8_t) * count;
+    if(getBuffRemainingBytes() < len )  {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer - can't allocate";
+        return false;
+    }
+    m_lock_order_counter__ = 0;
+    uint8_t *src = (uint8_t *)&m_available_spectrum_inquiry_response_obj[m_available_spectrum_inquiry_response_obj_idx__];
+    uint8_t *dst = src + len;
+    if (!m_parse__) {
+        size_t move_length = getBuffRemainingBytes(src) - len;
+        std::copy_n(src, move_length, dst);
+    }
+    m_available_spectrum_inquiry_response_obj_idx__ += count;
+    if (!buffPtrIncrementSafe(len)) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+        return false;
+    }
+    if(m_length){ (*m_length) += len; }
+    return true;
+}
+
+void tlvAvailableSpectrumInquiryResponse::class_swap()
+{
+    tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
+}
+
+bool tlvAvailableSpectrumInquiryResponse::finalize()
+{
+    if (m_parse__) {
+        TLVF_LOG(DEBUG) << "finalize() called but m_parse__ is set";
+        return true;
+    }
+    if (m_finalized__) {
+        TLVF_LOG(DEBUG) << "finalize() called for already finalized class";
+        return true;
+    }
+    if (!isPostInitSucceeded()) {
+        TLVF_LOG(ERROR) << "post init check failed";
+        return false;
+    }
+    if (m_inner__) {
+        if (!m_inner__->finalize()) {
+            TLVF_LOG(ERROR) << "m_inner__->finalize() failed";
+            return false;
+        }
+        auto tailroom = m_inner__->getMessageBuffLength() - m_inner__->getMessageLength();
+        m_buff_ptr__ -= tailroom;
+        *m_length -= tailroom;
+    }
+    class_swap();
+    m_finalized__ = true;
+    return true;
+}
+
+size_t tlvAvailableSpectrumInquiryResponse::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(eTlvTypeMap); // type
+    class_size += sizeof(uint16_t); // length
+    return class_size;
+}
+
+bool tlvAvailableSpectrumInquiryResponse::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_type = reinterpret_cast<eTlvTypeMap*>(m_buff_ptr__);
+    if (!m_parse__) *m_type = eTlvTypeMap::TLV_AVAILABLE_SPECTRUM_INQUIRY_RESPONSE;
+    if (!buffPtrIncrementSafe(sizeof(eTlvTypeMap))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(eTlvTypeMap) << ") Failed!";
+        return false;
+    }
+    m_length = reinterpret_cast<uint16_t*>(m_buff_ptr__);
+    if (!m_parse__) *m_length = 0;
+    if (!buffPtrIncrementSafe(sizeof(uint16_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint16_t) << ") Failed!";
+        return false;
+    }
+    m_available_spectrum_inquiry_response_obj = reinterpret_cast<uint8_t*>(m_buff_ptr__);
+    if (m_length && m_parse__) {
+        auto swap_len = *m_length;
+        tlvf_swap((sizeof(swap_len) * 8), reinterpret_cast<uint8_t*>(&swap_len));
+        size_t len = swap_len;
+        len -= (m_buff_ptr__ - sizeof(*m_type) - sizeof(*m_length) - m_buff__);
+        m_available_spectrum_inquiry_response_obj_idx__ = len/sizeof(uint8_t);
+        if (!buffPtrIncrementSafe(len)) {
+            LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+            return false;
+        }
+    }
+    if (m_parse__) { class_swap(); }
+    if (m_parse__) {
+        if (*m_type != eTlvTypeMap::TLV_AVAILABLE_SPECTRUM_INQUIRY_RESPONSE) {
+            TLVF_LOG(ERROR) << "TLV type mismatch. Expected value: " << int(eTlvTypeMap::TLV_AVAILABLE_SPECTRUM_INQUIRY_RESPONSE) << ", received value: " << int(*m_type);
+            return false;
+        }
+    }
+    return true;
+}
+
+
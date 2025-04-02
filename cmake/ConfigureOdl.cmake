984128507d1228a78bf1704c1651cc58c5f0409e
Volodymyr Pavlenko <pavlenko.vv@pm.me>
2025-04-02 17:17:49 +0300

    odl: refactor to have agent and controller folders
    Previously there was only one general odl folder,
    but now it's required to have separated odls
    for agent and controller
    
    Signed-off-by: Volodymyr Pavlenko <pavlenko.vv@pm.me>

--- cmake/ConfigureOdl.cmake
+++ cmake/ConfigureOdl.cmake
@@ -5,22 +5,28 @@
 # This code is subject to the terms of the BSD+Patent license.
 # See LICENSE file for more details.
 
-# The `configure_file_odl` function preprocesses .odl.in files.
+# The `configure_file_odl` function preprocesses .odl.in files. 
 # It temporarily replaces ${} to avoid its parsing by `configure_file`.
-function(configure_file_odl odl_file_input)
+function(configure_file_odl odl_file_input output_dir)
+    # Extract the directory and filename
     get_filename_component(input_directory ${odl_file_input} DIRECTORY)
     get_filename_component(input_filename ${odl_file_input} NAME)
 
+    # Get the filename without extension
     string(REGEX REPLACE "\\.[^.]*$" "" barename ${input_filename})
+
+    # Get the relative path from the source directory to the input directory
     file(RELATIVE_PATH relative_path ${CMAKE_CURRENT_SOURCE_DIR} ${input_directory})
-    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${relative_path}")
+
+    # Ensure the output directory exists
+    file(MAKE_DIRECTORY "${output_dir}/${relative_path}")
 
     # Replace ${} with $@@{} to prevent parsing by `configure_file`.
     execute_process(COMMAND bash "-c" "sed -r -i 's/\\$\\{([^}]*)\\}/\\$@@\\{\\1\\}/g' ${odl_file_input}")
 
-    message(STATUS "Generating ODL file: ${barename}")
-    configure_file(${odl_file_input} "${CMAKE_CURRENT_BINARY_DIR}/${relative_path}/${barename}")
+    message(STATUS  "${BoldCyan}Generating odl file${ColourReset} : " ${barename})
+    configure_file(${odl_file_input} "${output_dir}/${relative_path}/${barename}")
 
     # Revert $@@{} back to ${} after `configure_file`.
-    execute_process(COMMAND bash "-c" "sed -r -i 's/\\$@@\\{([^}]*)\\}/\\$\\{\\1\\}/g' ${CMAKE_CURRENT_BINARY_DIR}/${relative_path}/${barename}")
+    execute_process(COMMAND bash "-c" "sed -r -i 's/\\$@@\\{([^}]*)\\}/\\$\\{\\1\\}/g' ${output_dir}/${relative_path}/${barename}")
 endfunction()
